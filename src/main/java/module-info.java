4e02af7ce6c02b8619a05dde4f19ea1ac9b692b1
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-11 12:11:13 +0600

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower to javafx.fxml;
+    exports com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower;
+}
