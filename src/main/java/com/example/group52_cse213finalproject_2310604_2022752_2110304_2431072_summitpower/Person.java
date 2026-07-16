87ab76fcecf041c0b0eb52e41b7d7d0d8467b7ab
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-07-16 09:11:52 +0600

    Added Person, User, HRManager, and InventoryManager class

---
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/Person.java
@@ -0,0 +1,13 @@
+package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+    private String gender;
+
+    public void updateInformation(){
+
+    }
+}
---
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/User.java
@@ -0,0 +1,19 @@
+package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower;
+
+public class User extends Person {
+    private String userId;
+    private String email;
+    private String password;
+
+    public void login(){
+
+    }
+
+    public void logout(){
+
+    }
+
+    public void changePassword(){
+
+    }
+}
