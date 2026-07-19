3f6808a15b7a63b475471ceb4acb13e0e9b4b4d5
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-19 15:31:23 +0600

    made all the dic for fxml files in resourse & made two dashboard file for my two user.

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/controller/AccountantDashboardViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/controller/AccountantDashboardViewController.java
@@ -1,6 +1,6 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.controller;
 
-public class DashboardViewController
+public class AccountantDashboardViewController
 {
     @javafx.fxml.FXML
     public void initialize() {
--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/bpdbOfficer/controller/BpdbOfficerDashboardViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/bpdbOfficer/controller/BpdbOfficerDashboardViewController.java
@@ -1,4 +1,7 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.bpdbOfficer.controller;
 
-public class bpdbOfficerDashboardController {
-}
+public class BpdbOfficerDashboardViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
