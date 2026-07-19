1fe178729f1dc22975a42d77d351c970f87b92f3
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-20 01:00:34 +0600

    half validate loginController

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/commonFiles/LogInViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/commonFiles/LogInViewController.java
@@ -3,6 +3,7 @@ package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_s
 import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.PrimarySceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class LogInViewController
@@ -23,5 +24,18 @@ public class LogInViewController
 
     @javafx.fxml.FXML
     public void handleLogInButton(ActionEvent actionEvent) {
+        String userId = userIdTextField.getText();
+        String password = passwordTextField.getText();
+        if (userId.isEmpty()||password.isEmpty()){
+            showError("Please fill out all fields");
+            return;
+        }
+    }
+    public void showError(String txt){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(txt);
+        alert.showAndWait();
     }
 }
