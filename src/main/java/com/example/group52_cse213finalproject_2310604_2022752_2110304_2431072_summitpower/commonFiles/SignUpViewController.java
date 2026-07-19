0233aee90ade17f8ee3c8a73b41d263f8073b0e9
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-19 16:17:01 +0600

    wrote all the fx:id & handler method also updated the fxml files & added gen types for comboBox & imported all the packages

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/commonFiles/SignUpViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/commonFiles/SignUpViewController.java
@@ -1,7 +1,43 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.commonFiles;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class SignUpViewController
 {
     @javafx.fxml.FXML
+    private TextField passwordTextField;
+    @javafx.fxml.FXML
+    private TextField addressTextField;
+    @javafx.fxml.FXML
+    private TextField lastNameTextField;
+    @javafx.fxml.FXML
+    private TextField userIdTextField;
+    @javafx.fxml.FXML
+    private TextField emailTextField;
+    @javafx.fxml.FXML
+    private ComboBox <String> genderComboBox;
+    @javafx.fxml.FXML
+    private TextField firstNameTextField;
+    @javafx.fxml.FXML
+    private CheckBox termsCheckBox;
+    @javafx.fxml.FXML
+    private TextField phoneTextField1;
+    @javafx.fxml.FXML
+    private DatePicker dateOfBirthDatePicker;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleSignUpButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAlreadyHaveAnAccount(ActionEvent actionEvent) {
+    }
+}
