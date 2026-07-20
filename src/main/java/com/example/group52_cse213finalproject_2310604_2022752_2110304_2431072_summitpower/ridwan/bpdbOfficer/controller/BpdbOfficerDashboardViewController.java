f2864cad4573f3bfbd8d0f9fd998d5e111bfe09f
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-20 14:44:38 +0600

    added buttons to the bpdb Officer Dashboard

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/bpdbOfficer/controller/BpdbOfficerDashboardViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/bpdbOfficer/controller/BpdbOfficerDashboardViewController.java
@@ -1,7 +1,57 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.bpdbOfficer.controller;
 
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.PrimarySceneSwitcher;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.text.Text;
+
 public class BpdbOfficerDashboardViewController
 {
     @javafx.fxml.FXML
+    private Text officerNameText;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleSupplyAgreementButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAdditionalPowerButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleMonitorPowerSupplyButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSubmitPowerDemandButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleInvoiceButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogoutButton(ActionEvent actionEvent) {
+        PrimarySceneSwitcher.primarySwitchScene((Node) actionEvent.getSource(),"commonFiles","log-in-view.fxml", "Log in!");
+    }
+
+    @javafx.fxml.FXML
+    public void handleProfileButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleReviewContractPerformanceButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleGenerateSupplyReportButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSubmitComplaintButton(ActionEvent actionEvent) {
+    }
+}
