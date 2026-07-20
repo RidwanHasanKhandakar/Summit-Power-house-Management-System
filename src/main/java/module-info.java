f669c75b391b558ee93d03198e93e59c56cbd330
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-20 13:27:24 +0600

    Updated the module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,6 @@ module com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_su
 
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower to javafx.fxml;
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.commonFiles to javafx.fxml;
-    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant to javafx.fxml;
-    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.bpdbOfficer to javafx.fxml;
+    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.controller to javafx.fxml;
+    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.bpdbOfficer.controller to javafx.fxml;
 }
