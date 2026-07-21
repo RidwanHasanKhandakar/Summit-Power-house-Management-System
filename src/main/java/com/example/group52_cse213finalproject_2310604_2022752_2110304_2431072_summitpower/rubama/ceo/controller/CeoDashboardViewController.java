fba8d1a328dd7ef3acede69426b536871fd2d7be
rubamamehnaj <rubamamehnaj@gmail.com>
2026-07-22 01:04:19 +0600

    updated fxml and controller with fxids and codes

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/CeoDashboardViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/CeoDashboardViewController.java
@@ -1,7 +1,50 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class CeoDashboardViewController
 {
     @javafx.fxml.FXML
+    private TableColumn activityCol;
+    @javafx.fxml.FXML
+    private DatePicker dateDatePicker;
+    @javafx.fxml.FXML
+    private TextField searchDashboardTextField;
+    @javafx.fxml.FXML
+    private TextField revenueTextField;
+    @javafx.fxml.FXML
+    private Label filteredTableView;
+    @javafx.fxml.FXML
+    private ComboBox pendingRequestComboBox;
+    @javafx.fxml.FXML
+    private TableColumn performedByCol;
+    @javafx.fxml.FXML
+    private ComboBox userComboBox;
+    @javafx.fxml.FXML
+    private TableColumn dateCol;
+    @javafx.fxml.FXML
+    private TableColumn statusCol;
+    @javafx.fxml.FXML
+    private TableView ceoTableView;
+    @javafx.fxml.FXML
+    private ComboBox pendingComplaintsComboBox;
+    @javafx.fxml.FXML
+    private TextField totalGenerationTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleSearch(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleRefresh(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleLogOut(ActionEvent actionEvent) {
+    }
+}
