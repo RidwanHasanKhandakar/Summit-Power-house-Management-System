9717c9e5b84c04f61306cb51e6c8dcf6a42678fd
rubamamehnaj <rubamamehnaj@gmail.com>
2026-07-24 00:00:52 +0600

    updated an fxml

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/ComplaintSummaryViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/ComplaintSummaryViewController.java
@@ -1,7 +1,40 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class ComplaintSummaryViewController
 {
     @javafx.fxml.FXML
+    private Label viewComplaintSummaryLabel;
+    @javafx.fxml.FXML
+    private DatePicker dateOfComplaintDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn complaintIdCol;
+    @javafx.fxml.FXML
+    private TableColumn statusCol;
+    @javafx.fxml.FXML
+    private ComboBox statusComboBox;
+    @javafx.fxml.FXML
+    private TextField customerIdTextField;
+    @javafx.fxml.FXML
+    private TableColumn dateOfComplaintCol;
+    @javafx.fxml.FXML
+    private TableColumn customerIdCol;
+    @javafx.fxml.FXML
+    private TableView complaintTableView;
+    @javafx.fxml.FXML
+    private TableColumn descriptionCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleComplaintSummary(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleViewComplaints(ActionEvent actionEvent) {
+    }
+}
