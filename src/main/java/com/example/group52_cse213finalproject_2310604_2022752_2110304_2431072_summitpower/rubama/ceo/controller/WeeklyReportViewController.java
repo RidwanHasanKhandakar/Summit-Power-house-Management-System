42049ec858ffeb18cd70f70c5b1eb529678cdfe2
rubamamehnaj <rubamamehnaj@gmail.com>
2026-07-25 01:26:41 +0600

    added an fxml and updated controller

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/WeeklyReportViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/WeeklyReportViewController.java
@@ -1,7 +1,41 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class WeeklyReportViewController
 {
     @javafx.fxml.FXML
+    private TableColumn totalGenerationCol;
+    @javafx.fxml.FXML
+    private TableView performanceTableView;
+    @javafx.fxml.FXML
+    private TableColumn startingDateCol;
+    @javafx.fxml.FXML
+    private TableColumn totalPowerLossCol;
+    @javafx.fxml.FXML
+    private DatePicker startingDateDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn endingDateCol;
+    @javafx.fxml.FXML
+    private Label weeklyReportLabel;
+    @javafx.fxml.FXML
+    private DatePicker endingDateDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn performanceStatusCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleShowPerformance(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleGenerateWeeklyReport(ActionEvent actionEvent) {
+    }
+}
