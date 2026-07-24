35f8c4d369e312f468a55ea8f2dd5b465e28a307
rubamamehnaj <rubamamehnaj@gmail.com>
2026-07-24 23:50:03 +0600

    added an fxml and updated the controller

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/FuelPurchaseApprovalController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/FuelPurchaseApprovalController.java
@@ -1,7 +1,31 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class FuelPurchaseApprovalController
 {
     @javafx.fxml.FXML
+    private TableColumn requestsCol;
+    @javafx.fxml.FXML
+    private TableColumn approvalStatusCol;
+    @javafx.fxml.FXML
+    private TableView fuelRequestsTableView;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleAprove(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleApproveNot(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handlePendingFuelRequests(ActionEvent actionEvent) {
+    }
+}
