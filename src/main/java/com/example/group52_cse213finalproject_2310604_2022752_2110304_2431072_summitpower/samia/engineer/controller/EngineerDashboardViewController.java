6c5282d35f3115298942fd96f16ed1cf7ec2e43a
Samia Islam <2431072@iub.edu.bd>
2026-07-24 16:49:05 +0600

    i made preventive maintenance fxml controller and fixed generator status class and made dashboard view

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/engineer/controller/EngineerDashboardViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/engineer/controller/EngineerDashboardViewController.java
@@ -1,7 +1,54 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.engineer.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.text.Text;
+
 public class EngineerDashboardViewController
 {
     @javafx.fxml.FXML
+    private Text officerNameText;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void manageSparePartsReqButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void monitorFuelConsumptionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void equipmentEfficiencyButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void monitorGeneratorStatusButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void preventiveMaintenanceButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateMaintenanceStatusButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void profileButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void reportEquipmentFaultsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void engineeringPerformanceButton(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/engineer/controller/PreventiveMaintenanceViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/engineer/controller/PreventiveMaintenanceViewController.java
@@ -1,7 +1,69 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.engineer.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class PreventiveMaintenanceViewController
 {
     @javafx.fxml.FXML
+    private ComboBox<String> priorityComboBox;
+    @javafx.fxml.FXML
+    private ComboBox<String> maintenanceStatusComboBox;
+    @javafx.fxml.FXML
+    private TextField generatorIdTextField;
+    @javafx.fxml.FXML
+    private TextField maintenanceIdTextField;
+    @javafx.fxml.FXML
+    private ComboBox<String> maintenanceTypeComboBox;
+    @javafx.fxml.FXML
+    private DatePicker scheduledDatePicker;
+    @javafx.fxml.FXML
+    private ComboBox<String> plantUnitComboBox;
+    @javafx.fxml.FXML
+    private TextArea remarksTextArea;
+    @javafx.fxml.FXML
+    private TextField engineerNameTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+        plantUnitComboBox.getItems().addAll(
+                "Unit 1",
+                "Unit 2",
+                "Unit 3",
+                "Unit 4"
+        );
+        maintenanceTypeComboBox.getItems().addAll(
+                "Inspection",
+                "Oil Change",
+                "Filter Replacement",
+                "Cleaning",
+                "Testing"
+        );
+        priorityComboBox.getItems().addAll(
+                "Low",
+                "Medium",
+                "High"
+        );
+        maintenanceStatusComboBox.getItems().addAll(
+                "Pending",
+                "In Progress",
+                "Completed"
+        );
+
+    }
+
+    @javafx.fxml.FXML
+    public void refreshButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveButton(ActionEvent actionEvent) {
+    }
+}
