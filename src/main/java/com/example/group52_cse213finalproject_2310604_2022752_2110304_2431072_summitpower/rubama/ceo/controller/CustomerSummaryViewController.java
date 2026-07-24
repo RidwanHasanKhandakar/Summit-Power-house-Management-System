52600e2eaf958e93ec1a5b95cc2975af11fd8bc0
rubamamehnaj <rubamamehnaj@gmail.com>
2026-07-24 22:08:11 +0600

    added an fxml and updated the controller

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/CustomerSummaryViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/CustomerSummaryViewController.java
@@ -1,7 +1,32 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CustomerSummaryViewController
 {
     @javafx.fxml.FXML
+    private DatePicker fromDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn dateCol;
+    @javafx.fxml.FXML
+    private TableColumn industrialCustomersCol;
+    @javafx.fxml.FXML
+    private DatePicker toDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn residentialCustomersCol;
+    @javafx.fxml.FXML
+    private TableView customerSummaryTableView;
+    @javafx.fxml.FXML
+    private TableColumn commercialCustomersCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleShowCustomerSummary(ActionEvent actionEvent) {
+    }
+}
