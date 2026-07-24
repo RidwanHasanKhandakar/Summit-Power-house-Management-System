6afc91c675bd940bd1c035aee70322bf36320e7c
rubamamehnaj <rubamamehnaj@gmail.com>
2026-07-24 22:44:38 +0600

    added an fxml and updated a controller class

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/EmployeeManagementViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/EmployeeManagementViewController.java
@@ -1,7 +1,37 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class EmployeeManagementViewController
 {
     @javafx.fxml.FXML
+    private TableView employeeTableView;
+    @javafx.fxml.FXML
+    private TableColumn employeeNameCol;
+    @javafx.fxml.FXML
+    private TextField employeeNameTextField;
+    @javafx.fxml.FXML
+    private ComboBox roleComboBox;
+    @javafx.fxml.FXML
+    private TableColumn roleCol;
+    @javafx.fxml.FXML
+    private TableColumn employeeIdCol;
+    @javafx.fxml.FXML
+    private TextField employeeIdTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleRemoveEmployee(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleAddEmployee(ActionEvent actionEvent) {
+    }
+}
