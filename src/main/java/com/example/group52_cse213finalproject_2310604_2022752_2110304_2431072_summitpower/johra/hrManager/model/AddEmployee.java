8aeb921403355dcf6b0b4d9780475a3dcfea94c5
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-07-27 00:27:03 +0600

    Deleted Search employee fxml and controller and added Employee Training Management

---
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/hrManager/model/AddEmployee.java
@@ -0,0 +1,11 @@
+package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.hrManager.model;
+
+public class AddEmployee {
+    private String employeeId, employeeName, phone, email, address, gender,
+            department, position, religion, maritalStatus;
+    private localDate dateOfBirth, dateOfJoining;
+    private int salary;
+
+
+
+}
