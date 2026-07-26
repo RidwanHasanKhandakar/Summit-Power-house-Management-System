c1f792e6fdd02ab54c53b448e7c1b746da6d4757
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-26 21:30:56 +0600

    fixed the file path in invoice & powerSupplymonitor

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/bpdbOfficer/controller/InvoicesViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/bpdbOfficer/controller/InvoicesViewController.java
@@ -60,7 +60,7 @@ public class InvoicesViewController
 
     @javafx.fxml.FXML
     public void handleHomeButton(ActionEvent actionEvent) {
-        PrimarySceneSwitcher.primarySwitchScene((Node) actionEvent.getSource(),"bpdbOfficer", "bpdbOfficer-dashboard-view.fxml", "BPDB Officer Dashboard!");
+        PrimarySceneSwitcher.primarySwitchScene((Node) actionEvent.getSource(),"ridwan", "bpdbOfficer", "bpdbOfficer-dashboard-view.fxml","BPDB Officer Dashboard!");
     }
 
     @javafx.fxml.FXML
