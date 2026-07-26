3da77932cab8189eadaa2bda5a2343f1f42ff8ff
Samia Islam <2431072@iub.edu.bd>
2026-07-27 00:32:27 +0600

    created monitor grid status model and fxml

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/gridOperator/controller/MonitorGridStatusViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/gridOperator/controller/MonitorGridStatusViewController.java
@@ -1,7 +1,50 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.gridOperator.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class MonitorGridStatusViewController
 {
     @javafx.fxml.FXML
+    private TextField gridIdTextField;
+    @javafx.fxml.FXML
+    private DatePicker monitoringDatePicker;
+    @javafx.fxml.FXML
+    private ComboBox<String> gridStatusComboBox;
+    @javafx.fxml.FXML
+    private TextField frequencyTextField;
+    @javafx.fxml.FXML
+    private ComboBox<String> gridSectionComboBox;
+    @javafx.fxml.FXML
+    private TextField voltageTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+        gridSectionComboBox.getItems().addAll(
+                "North Grid",
+                "South Grid",
+                "East Grid",
+                "West Grid"
+        );
+
+        gridStatusComboBox.getItems().addAll(
+                "Normal",
+                "Maintenance",
+                "Fault"
+        );
+    }
+
+    @javafx.fxml.FXML
+    public void searchButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void refreshButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+}
