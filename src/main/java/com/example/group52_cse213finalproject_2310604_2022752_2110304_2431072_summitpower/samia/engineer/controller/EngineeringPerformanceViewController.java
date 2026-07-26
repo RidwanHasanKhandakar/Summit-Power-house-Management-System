b63ebd7ee570ca8bdeee43c24c998f252de21ebb
Samia Islam <2431072@iub.edu.bd>
2026-07-26 23:39:17 +0600

    modified engineering performane controller

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/engineer/controller/EngineeringPerformanceViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/engineer/controller/EngineeringPerformanceViewController.java
@@ -1,9 +1,9 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.engineer.controller;
 
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.engineer.model.EngineeringPerformance;
 import javafx.event.ActionEvent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class EngineeringPerformanceViewController
 {
@@ -19,15 +19,46 @@ public class EngineeringPerformanceViewController
     private TextField generatorIdTextField;
     @javafx.fxml.FXML
     private TextField efficiencyTextField;
+    @javafx.fxml.FXML
+    private TableColumn<EngineeringPerformance,String> performanceStatusTabCol;
+    @javafx.fxml.FXML
+    private TableColumn<EngineeringPerformance,String> generatorIdTabCol;
+    @javafx.fxml.FXML
+    private TableColumn<EngineeringPerformance,String > reportIdTabCol;
+    @javafx.fxml.FXML
+    private TableView<EngineeringPerformance> tableView;
+    @javafx.fxml.FXML
+    private TableColumn<EngineeringPerformance,Double> efficiencyTabCol;
+    @javafx.fxml.FXML
+    private TableColumn<EngineeringPerformance,Double> powerOutputTabCol;
 
     @javafx.fxml.FXML
     public void initialize() {
+
+        efficiencyTextField.setEditable(false);
+
         performanceStatusComboBox.getItems().addAll(
                 "Excellent",
                 "Good",
                 "Average",
                 "Poor"
         );
+
+        reportIdTabCol.setCellValueFactory(
+                new PropertyValueFactory<>("reportId"));
+
+        generatorIdTabCol.setCellValueFactory(
+                new PropertyValueFactory<>("generatorId"));
+
+        powerOutputTabCol.setCellValueFactory(
+                new PropertyValueFactory<>("powerOutput"));
+
+        efficiencyTabCol.setCellValueFactory(
+                new PropertyValueFactory<>("efficiencyPercentage"));
+
+        performanceStatusTabCol.setCellValueFactory(
+                new PropertyValueFactory<>("performanceStatus"));
+
     }
 
     @javafx.fxml.FXML
