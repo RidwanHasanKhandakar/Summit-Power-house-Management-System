18cf68ed61753f7550933a5f03d2878797b0036a
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-27 19:42:50 +0600

    initialized the comboBox & fixed some of the gen IDS

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/controller/FuelInventoryViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/controller/FuelInventoryViewController.java
@@ -1,39 +1,66 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.controller;
 
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.model.FuelInventory;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+
+import java.time.LocalDate;
 
 public class FuelInventoryViewController
 {
     @javafx.fxml.FXML
     private Label totalInventoryValueLabel;
     @javafx.fxml.FXML
-    private ComboBox fuelTypeComboBox;
+    private ComboBox <String> fuelTypeComboBox;
     @javafx.fxml.FXML
-    private TableColumn lastUpdatedCol;
+    private TableColumn <FuelInventory, LocalDate> lastUpdatedCol;
     @javafx.fxml.FXML
     private Label avrgUnitCostLabel;
     @javafx.fxml.FXML
-    private TableColumn unitCostCol;
+    private TableColumn <FuelInventory,Double> unitCostCol;
     @javafx.fxml.FXML
     private DatePicker toDateDatePicker;
     @javafx.fxml.FXML
     private DatePicker fromDateDatePicker;
     @javafx.fxml.FXML
-    private TableView fuelInventoryValuationTableView;
+    private TableView <FuelInventory> fuelInventoryValuationTableView;
     @javafx.fxml.FXML
-    private TableColumn totalValueCol;
+    private TableColumn <FuelInventory,Double> totalValueCol;
     @javafx.fxml.FXML
     private Label totalQuantityLabel;
     @javafx.fxml.FXML
-    private TableColumn quantityCol;
+    private TableColumn <FuelInventory,Integer> quantityCol;
     @javafx.fxml.FXML
-    private TableColumn fuelIDCol;
+    private TableColumn <FuelInventory,String> fuelIDCol;
     @javafx.fxml.FXML
-    private TableColumn fuelTypeCol;
+    private TableColumn <FuelInventory,String> fuelTypeCol;
 
     @javafx.fxml.FXML
     public void initialize() {
+
+        fuelTypeComboBox.getItems().addAll(
+                "All",
+                "Diesel",
+                "Furnace Oil",
+                "Natural Gas",
+                "Coal",
+                "LPG",
+                "Petrol",
+                "Kerosene",
+                "Biofuel",
+                "Ethanol",
+                "Biodiesel",
+                "Hydrogen",
+                "Methanol",
+                "Propane",
+                "Butane",
+                "Wood",
+                "Peat",
+                "Charcoal"
+                );
+
+
+
     }
 
     @javafx.fxml.FXML
