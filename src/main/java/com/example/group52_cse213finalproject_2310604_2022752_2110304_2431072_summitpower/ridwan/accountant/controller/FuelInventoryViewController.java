1b263e0750bf6093423eee4baecfe47d8774cb1b
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-27 19:38:34 +0600

    updated the fxml file for fuel inventory

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/controller/FuelInventoryViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/controller/FuelInventoryViewController.java
@@ -1,7 +1,54 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class FuelInventoryViewController
 {
     @javafx.fxml.FXML
+    private Label totalInventoryValueLabel;
+    @javafx.fxml.FXML
+    private ComboBox fuelTypeComboBox;
+    @javafx.fxml.FXML
+    private TableColumn lastUpdatedCol;
+    @javafx.fxml.FXML
+    private Label avrgUnitCostLabel;
+    @javafx.fxml.FXML
+    private TableColumn unitCostCol;
+    @javafx.fxml.FXML
+    private DatePicker toDateDatePicker;
+    @javafx.fxml.FXML
+    private DatePicker fromDateDatePicker;
+    @javafx.fxml.FXML
+    private TableView fuelInventoryValuationTableView;
+    @javafx.fxml.FXML
+    private TableColumn totalValueCol;
+    @javafx.fxml.FXML
+    private Label totalQuantityLabel;
+    @javafx.fxml.FXML
+    private TableColumn quantityCol;
+    @javafx.fxml.FXML
+    private TableColumn fuelIDCol;
+    @javafx.fxml.FXML
+    private TableColumn fuelTypeCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void handleHomeButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleRefreshButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleCalculateViewsButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleFilterButton(ActionEvent actionEvent) {
+    }
+}
