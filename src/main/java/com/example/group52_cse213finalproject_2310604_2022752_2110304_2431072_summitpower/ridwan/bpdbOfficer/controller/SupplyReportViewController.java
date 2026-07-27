6530da1279c73643dedb91acca054c90b41019b7
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-27 21:56:28 +0600

    fixed all gen types and inited the comboBox for supplyReport

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/bpdbOfficer/controller/SupplyReportViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/bpdbOfficer/controller/SupplyReportViewController.java
@@ -1,7 +1,62 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.bpdbOfficer.controller;
 
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.bpdbOfficer.model.SupplyReport;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
 public class SupplyReportViewController
 {
     @javafx.fxml.FXML
+    private TableColumn <SupplyReport, String> reportIDCol;
+    @javafx.fxml.FXML
+    private TableColumn <SupplyReport,Double> genPowerCol;
+    @javafx.fxml.FXML
+    private TableColumn <SupplyReport,Double> LossCol;
+    @javafx.fxml.FXML
+    private TableView <SupplyReport> supplyReportTableView;
+    @javafx.fxml.FXML
+    private TableColumn <SupplyReport,String> reportTypeCol;
+    @javafx.fxml.FXML
+    private TableColumn <SupplyReport, LocalDate> reportDateCol;
+    @javafx.fxml.FXML
+    private TextArea viewSummaryTextArea;
+    @javafx.fxml.FXML
+    private ComboBox <String> reportTypeComboBox;
+    @javafx.fxml.FXML
+    private TableColumn <SupplyReport,Double> suppliedPowerCol;
+    @javafx.fxml.FXML
+    private DatePicker reportDateDatePicker;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+
+        reportTypeComboBox.getItems().addAll(
+                "All",
+                "Daily",
+                "Weekly",
+                "Monthly",
+                "Quarterly",
+                "Yearly"
+        );
+
+    }
+
+    @javafx.fxml.FXML
+    public void handleHomeButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleViewSummaryButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleSearchButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleRefreshButton(ActionEvent actionEvent) {
+    }
+}
