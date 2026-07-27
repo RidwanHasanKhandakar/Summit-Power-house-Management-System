0c9a8f404a61c4a2d1658ed68be2d13bc5dfc1e8
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-27 23:44:32 +0600

    added scene switcher to the home button to all controller classes

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/controller/FinancialStatementsViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/controller/FinancialStatementsViewController.java
@@ -1,30 +1,35 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.controller;
 
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.PrimarySceneSwitcher;
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.model.FinancialStatement;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+
+//import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Node;
 
 public class FinancialStatementsViewController
 {
     @javafx.fxml.FXML
-    private TableView finalcialStatementTableView;
+    private TableView <FinancialStatement> finalcialStatementTableView;
     @javafx.fxml.FXML
     private Label totalRevLabel;
     @javafx.fxml.FXML
     private DatePicker fromDateDatePicker;
     @javafx.fxml.FXML
-    private TableColumn amountCol;
+    private TableColumn <FinancialStatement,Double> amountCol;
     @javafx.fxml.FXML
     private ComboBox <String> statementTypeComboBox;
     @javafx.fxml.FXML
     private Label netProfitLabel;
     @javafx.fxml.FXML
-    private TableColumn categoryCol;
+    private TableColumn <FinancialStatement,String> categoryCol;
     @javafx.fxml.FXML
     private TextArea statementPreviewTextArea;
     @javafx.fxml.FXML
     private DatePicker toDateDatePicker;
     @javafx.fxml.FXML
-    private TableColumn accountCol;
+    private TableColumn <FinancialStatement,String> accountCol;
     @javafx.fxml.FXML
     private Label totalExpensesLabel;
 
@@ -46,6 +51,9 @@ public class FinancialStatementsViewController
 
     @javafx.fxml.FXML
     public void handleHomeButton(ActionEvent actionEvent) {
+
+        PrimarySceneSwitcher.primarySwitchScene((Node) actionEvent.getSource(),"ridwan","accountant","AccountantDashboardView.fxml","Accountant Dashboard");
+
     }
 
     @javafx.fxml.FXML
