fd75391072d12cee7746af8e63a854eb94cc2837
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-07-28 02:50:48 +0600

    Updated Add Employee fxml and controller for inventoryManager

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/hrManager/model/AddEmployee.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/hrManager/model/AddEmployee.java
@@ -1,9 +1,12 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.hrManager.model;
 
-public class AddEmployee {
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class AddEmployee implements Serializable {
     private String employeeId, employeeName, phone, email, address, gender,
             department, position, religion, maritalStatus;
-    private localDate dateOfBirth, dateOfJoining;
+    private LocalDate dateOfBirth, dateOfJoining;
     private int salary;
 
 
