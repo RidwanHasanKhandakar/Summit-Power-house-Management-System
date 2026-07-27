ce0fe4bedd576ab6b14257c7d477753a32370347
Samia Islam <2431072@iub.edu.bd>
2026-07-28 01:12:35 +0600

    created syncro and grid fault model and fxml

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/gridOperator/controller/SynchronizeGenWNationalGridViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/gridOperator/controller/SynchronizeGenWNationalGridViewController.java
@@ -1,7 +1,58 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.gridOperator.controller;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 public class SynchronizeGenWNationalGridViewController
 {
     @javafx.fxml.FXML
+    private DatePicker synchronizationDatePicker;
+    @javafx.fxml.FXML
+    private TextField synchronizationIdTextField;
+    @javafx.fxml.FXML
+    private TextField generatorIdTextField;
+    @javafx.fxml.FXML
+    private TextField gridVoltageTextField;
+    @javafx.fxml.FXML
+    private TextField generatorVoltageTextField;
+    @javafx.fxml.FXML
+    private ComboBox<String> synchronizationStatusComboBox;
+    @javafx.fxml.FXML
+    private ComboBox<String> gridSectionComboBox;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+        gridSectionComboBox.getItems().addAll(
+                "North Grid",
+                "South Grid",
+                "East Grid",
+                "West Grid"
+        );
+        synchronizationStatusComboBox.getItems().addAll(
+                "Successful",
+                "Pending",
+                "Failed"
+        );
+
+    }
+
+    @javafx.fxml.FXML
+    public void refreshButton(ActionEvent actionEvent) {
+        synchronizationIdTextField.clear();
+        generatorIdTextField.clear();
+        gridSectionComboBox.setValue(null);
+        generatorVoltageTextField.clear();
+        gridVoltageTextField.clear();
+        synchronizationStatusComboBox.setValue(null);
+        synchronizationDatePicker.setValue(null);
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveButton(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/gridOperator/model/RespondToGridFault.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/gridOperator/model/RespondToGridFault.java
@@ -1,6 +1,83 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.gridOperator.model;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class RespondToGridFault implements Serializable {
+
+    private String faultId,gridSection,faultType;
+    private LocalDate faultDate;
+    private String responseAction,faultStatus;
+
+    public RespondToGridFault() {
+    }
+
+    public RespondToGridFault(String faultId, String gridSection, String faultType, LocalDate faultDate, String responseAction, String faultStatus) {
+        this.faultId = faultId;
+        this.gridSection = gridSection;
+        this.faultType = faultType;
+        this.faultDate = faultDate;
+        this.responseAction = responseAction;
+        this.faultStatus = faultStatus;
+    }
+
+    public String getFaultId() {
+        return faultId;
+    }
+
+    public void setFaultId(String faultId) {
+        this.faultId = faultId;
+    }
+
+    public String getGridSection() {
+        return gridSection;
+    }
+
+    public void setGridSection(String gridSection) {
+        this.gridSection = gridSection;
+    }
+
+    public String getFaultType() {
+        return faultType;
+    }
+
+    public void setFaultType(String faultType) {
+        this.faultType = faultType;
+    }
+
+    public LocalDate getFaultDate() {
+        return faultDate;
+    }
+
+    public void setFaultDate(LocalDate faultDate) {
+        this.faultDate = faultDate;
+    }
+
+    public String getResponseAction() {
+        return responseAction;
+    }
+
+    public void setResponseAction(String responseAction) {
+        this.responseAction = responseAction;
+    }
+
+    public String getFaultStatus() {
+        return faultStatus;
+    }
+
+    public void setFaultStatus(String faultStatus) {
+        this.faultStatus = faultStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "RespondToGridFault{" +
+                "faultId='" + faultId + '\'' +
+                ", gridSection='" + gridSection + '\'' +
+                ", faultType='" + faultType + '\'' +
+                ", faultDate=" + faultDate +
+                ", responseAction='" + responseAction + '\'' +
+                ", faultStatus='" + faultStatus + '\'' +
+                '}';
+    }
 }
