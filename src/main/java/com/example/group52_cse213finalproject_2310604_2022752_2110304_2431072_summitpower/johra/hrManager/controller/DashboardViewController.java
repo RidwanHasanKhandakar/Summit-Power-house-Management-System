1b308ad2b5e282ff23f3c2157c34c7c468cb6023
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-07-28 23:56:54 +0600

    Added fxid to Dashboard fxml and updated controller hrManager

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/hrManager/controller/DashboardViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/hrManager/controller/DashboardViewController.java
@@ -1,7 +1,50 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.hrManager.controller;
 
+import javafx.event.ActionEvent;
+
 public class DashboardViewController
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void managePromotionButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void manageEmployeeTrainingButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void profileButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewEmployeeDetailsButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void performanceGradingButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void candidateSelectionButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addEmployeeButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createNoticeButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeCurrentEmployeeOnAction(ActionEvent actionEvent) {
+    }
+}
