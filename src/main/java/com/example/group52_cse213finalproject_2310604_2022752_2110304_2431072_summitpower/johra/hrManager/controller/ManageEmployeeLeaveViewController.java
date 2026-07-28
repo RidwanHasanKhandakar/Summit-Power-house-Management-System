4eb9e456860ad365aaf4b7c774b1926d8f3b5a38
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-07-28 08:49:43 +0600

    Renamed fxml files and updated controller classes

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/hrManager/controller/ManageEmployeeLeaveViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/hrManager/controller/ManageEmployeeLeaveViewController.java
@@ -1,6 +1,6 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.hrManager.controller;
 
-public class ManageEmployeeViewController
+public class ManageEmployeeLeaveViewController
 {
     @javafx.fxml.FXML
     public void initialize() {
