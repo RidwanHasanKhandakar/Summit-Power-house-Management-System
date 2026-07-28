30f0433a57819c54182e0cb291d8332f63c771cd
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-28 13:51:44 +0600

    fixed the gen type & inited the cols for trackfuel

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
     exports com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower;
 
