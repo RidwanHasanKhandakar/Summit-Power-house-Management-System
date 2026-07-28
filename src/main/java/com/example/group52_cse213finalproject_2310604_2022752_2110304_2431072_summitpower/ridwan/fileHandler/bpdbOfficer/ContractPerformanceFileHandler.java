d804aa5d857d6744cc140adcaf8ba3023c87fc59
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-28 23:23:39 +0600

    created a new bin file contract performance and wrote code for file hanlder

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/fileHandler/bpdbOfficer/ContractPerformanceFileHandler.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/fileHandler/bpdbOfficer/ContractPerformanceFileHandler.java
@@ -1,4 +1,48 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.fileHandler.bpdbOfficer;
 
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.bpdbOfficer.model.Complaint;
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.bpdbOfficer.model.ContractPerformance;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class ContractPerformanceFileHandler {
+
+    private static final String FILE_PATH = "data/ridwan/bpdbOfficer/contractPerformance.txt";
+
+    public static void save(ContractPerformance contractPerformance) {
+        ObservableList<ContractPerformance> list = readAll();
+        list.add(contractPerformance);
+
+        try{
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH));
+            oos.writeObject(new ArrayList<>(list));
+            oos.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static ObservableList<ContractPerformance> readAll() {
+        File file = new File(FILE_PATH);
+
+        if (!file.exists()||file.length()==0) {
+            return FXCollections.observableArrayList();
+        }
+
+        try{
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH));
+            ArrayList<ContractPerformance> list = (ArrayList<ContractPerformance>) ois.readObject();
+            ois.close();
+            return FXCollections.observableArrayList(list);
+        }catch (Exception e){
+            e.printStackTrace();
+            return FXCollections.observableArrayList();
+        }
+
+    }
+
 }
