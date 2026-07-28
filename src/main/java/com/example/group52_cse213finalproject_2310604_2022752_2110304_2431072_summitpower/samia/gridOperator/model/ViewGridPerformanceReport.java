3193de22f7ff7acbab28f62cddad43fc699cfc46
Samia Islam <2431072@iub.edu.bd>
2026-07-28 13:26:43 +0600

    made model class of grid performance

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/gridOperator/model/ViewGridPerformanceReport.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/gridOperator/model/ViewGridPerformanceReport.java
@@ -1,6 +1,94 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.gridOperator.model;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class ViewGridPerformanceReport implements Serializable {
+
+    private String reportId,gridSection;
+    private double totalLoadMW,availableCapacityMW,gridEfficiency;
+    private String performanceStatus;
+    private LocalDate reportDate;
+
+    public ViewGridPerformanceReport() {
+    }
+
+    public ViewGridPerformanceReport(String reportId, String gridSection, double totalLoadMW, double availableCapacityMW, double gridEfficiency, String performanceStatus, LocalDate reportDate) {
+        this.reportId = reportId;
+        this.gridSection = gridSection;
+        this.totalLoadMW = totalLoadMW;
+        this.availableCapacityMW = availableCapacityMW;
+        this.gridEfficiency = gridEfficiency;
+        this.performanceStatus = performanceStatus;
+        this.reportDate = reportDate;
+    }
+
+    public String getReportId() {
+        return reportId;
+    }
+
+    public void setReportId(String reportId) {
+        this.reportId = reportId;
+    }
+
+    public String getGridSection() {
+        return gridSection;
+    }
+
+    public void setGridSection(String gridSection) {
+        this.gridSection = gridSection;
+    }
+
+    public double getTotalLoadMW() {
+        return totalLoadMW;
+    }
+
+    public void setTotalLoadMW(double totalLoadMW) {
+        this.totalLoadMW = totalLoadMW;
+    }
+
+    public double getAvailableCapacityMW() {
+        return availableCapacityMW;
+    }
+
+    public void setAvailableCapacityMW(double availableCapacityMW) {
+        this.availableCapacityMW = availableCapacityMW;
+    }
+
+    public double getGridEfficiency() {
+        return gridEfficiency;
+    }
+
+    public void setGridEfficiency(double gridEfficiency) {
+        this.gridEfficiency = gridEfficiency;
+    }
+
+    public String getPerformanceStatus() {
+        return performanceStatus;
+    }
+
+    public void setPerformanceStatus(String performanceStatus) {
+        this.performanceStatus = performanceStatus;
+    }
+
+    public LocalDate getReportDate() {
+        return reportDate;
+    }
+
+    public void setReportDate(LocalDate reportDate) {
+        this.reportDate = reportDate;
+    }
+
+    @Override
+    public String toString() {
+        return "ViewGridPerformanceReport{" +
+                "reportId='" + reportId + '\'' +
+                ", gridSection='" + gridSection + '\'' +
+                ", totalLoadMW=" + totalLoadMW +
+                ", availableCapacityMW=" + availableCapacityMW +
+                ", gridEfficiency=" + gridEfficiency +
+                ", performanceStatus='" + performanceStatus + '\'' +
+                ", reportDate=" + reportDate +
+                '}';
+    }
 }
