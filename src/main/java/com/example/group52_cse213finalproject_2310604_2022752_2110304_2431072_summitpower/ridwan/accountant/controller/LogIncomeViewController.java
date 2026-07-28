0e28e26d510bb53ac28f3c8eec4666b198fe4645
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-28 13:24:31 +0600

    did the col init for log income & fixed the gen types

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/controller/LogIncomeViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/controller/LogIncomeViewController.java
@@ -1,41 +1,52 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.controller;
 
 import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.PrimarySceneSwitcher;
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.model.LogIncome;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
+
+import java.time.LocalDate;
 
 public class LogIncomeViewController
 {
     @javafx.fxml.FXML
-    private TableView salesIncomeTableView;
+    private TableView <LogIncome> salesIncomeTableView;
     @javafx.fxml.FXML
     private DatePicker dateDatePicker;
     @javafx.fxml.FXML
-    private TableColumn dateCol;
+    private TableColumn <LogIncome, LocalDate> dateCol;
     @javafx.fxml.FXML
-    private TableColumn clientNameCol;
+    private TableColumn <LogIncome,String> clientNameCol;
     @javafx.fxml.FXML
     private TextField clientCompanyTextField;
     @javafx.fxml.FXML
     private TextField revenueTextField;
     @javafx.fxml.FXML
-    private TableColumn unitsCol;
+    private TableColumn <LogIncome,Double> unitsCol;
     @javafx.fxml.FXML
     private TextField soldTextField;
     @javafx.fxml.FXML
-    private TableColumn salesIDCol;
+    private TableColumn <LogIncome,Integer> salesIDCol;
     @javafx.fxml.FXML
-    private TableColumn revenueCol;
+    private TableColumn <LogIncome,Double> revenueCol;
     @javafx.fxml.FXML
     private Text totalRevenueText;
 
     @javafx.fxml.FXML
     public void initialize() {
+
+        salesIDCol.setCellValueFactory(new PropertyValueFactory<>("saleID"));
+        clientNameCol.setCellValueFactory(new PropertyValueFactory<>("clientName"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("transactionDate"));
+        unitsCol.setCellValueFactory(new PropertyValueFactory<>("unitsSold"));
+        revenueCol.setCellValueFactory(new PropertyValueFactory<>("revenueAmount"));
+
     }
 
     @javafx.fxml.FXML
