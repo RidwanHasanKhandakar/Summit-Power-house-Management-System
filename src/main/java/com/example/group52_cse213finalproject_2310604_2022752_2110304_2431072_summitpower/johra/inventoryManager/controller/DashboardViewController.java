1589f97d2da1fbedbbb9322c44ac07bf04ac0d06
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-07-30 07:51:10 +0600

    Updated view for Inventory Report fxml and updated controller class for Add Equipment, DashBoard and Inventory Report

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/controller/DashboardViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/controller/DashboardViewController.java
@@ -1,5 +1,7 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.inventoryManager.controller;
 
+import javafx.event.ActionEvent;
+
 public class DashboardViewController
 {
     @javafx.fxml.FXML
