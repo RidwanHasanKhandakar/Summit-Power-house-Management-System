fcbbbe9bb66514ee56be2259c47c93beb769e2e8
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-07-30 07:55:48 +0600

    Added fxid and updated controller class for Offer Tender

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/controller/OfferTenderViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/controller/OfferTenderViewController.java
@@ -3,5 +3,33 @@ package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_s
 public class OfferTenderViewController
 {
     @javafx.fxml.FXML
+    private TextField equipmentNameTextField;
+    @javafx.fxml.FXML
+    private TableColumn equipmentCol;
+    @javafx.fxml.FXML
+    private TableColumn closingDateCol;
+    @javafx.fxml.FXML
+    private TextField tenderIdTextField;
+    @javafx.fxml.FXML
+    private DatePicker closingDateDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn tenderIdCol;
+    @javafx.fxml.FXML
+    private TextField tenderTitleTextField;
+    @javafx.fxml.FXML
+    private TableView showTenderTableView;
+    @javafx.fxml.FXML
+    private TableColumn tenderTitleCol;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void homeButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createTenderButtonOnAction(ActionEvent actionEvent) {
+    }
+}
