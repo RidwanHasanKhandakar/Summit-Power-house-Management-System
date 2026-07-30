591ad5c198b1645e8fd30036960bce0801a5018e
rubamamehnaj <rubamamehnaj@gmail.com>
2026-07-31 01:23:52 +0600

    added sceneswitching

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/FuelPurchaseApprovalController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/FuelPurchaseApprovalController.java
@@ -1,6 +1,8 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.controller;
 
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.PrimarySceneSwitcher;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
@@ -31,5 +33,7 @@ public class FuelPurchaseApprovalController
 
     @javafx.fxml.FXML
     public void handleReturn(ActionEvent actionEvent) {
+        public void handleReturn(ActionEvent actionEvent) {
+            PrimarySceneSwitcher.primarySwitchScene((Node) actionEvent.getSource(),"rubama","ceo","ceo-dashboard-view.fxml","CEO Dashboard");
     }
 }
