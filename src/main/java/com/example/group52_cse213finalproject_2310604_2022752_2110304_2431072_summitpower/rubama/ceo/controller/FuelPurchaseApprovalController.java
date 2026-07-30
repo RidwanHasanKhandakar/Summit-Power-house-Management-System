afb53289b511bb249f527ab6b8bf59ea9c5c72f2
rubamamehnaj <rubamamehnaj@gmail.com>
2026-07-31 02:21:24 +0600

    added model class and updated controller

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/FuelPurchaseApprovalController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/controller/FuelPurchaseApprovalController.java
@@ -1,25 +1,29 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.controller;
 
 import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.PrimarySceneSwitcher;
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.model.FuelPurchaseApproval;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class FuelPurchaseApprovalController
 {
     @javafx.fxml.FXML
-    private TableColumn requestsCol;
+    private TableColumn <FuelPurchaseApproval,String>  requestsCol;
     @javafx.fxml.FXML
-    private TableColumn approvalStatusCol;
+    private TableColumn <FuelPurchaseApproval,String> approvalStatusCol;
     @javafx.fxml.FXML
-    private TableView fuelRequestsTableView;
+    private TableView <FuelPurchaseApproval> fuelRequestsTableView;
 
     @javafx.fxml.FXML
     public void initialize() {
+        approvalStatusCol.setCellValueFactory(new PlantPerformanceViewController("requests"));
+        requestsCol.setCellValueFactory(new PropertyValueFactory<>("approvalStatus"));
     }
 
-    @javafx.fxml.FXML
+    @Deprecated
     public void handleAprove(ActionEvent actionEvent) {
     }
 
@@ -34,5 +38,9 @@ public class FuelPurchaseApprovalController
     @javafx.fxml.FXML
     public void handleReturn(ActionEvent actionEvent) {
             PrimarySceneSwitcher.primarySwitchScene((Node) actionEvent.getSource(),"rubama","ceo","ceo-dashboard-view.fxml","CEO Dashboard");
+    }
+
+    @javafx.fxml.FXML
+    public void handleApprove(ActionEvent actionEvent) {
     }
 }
--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/model/FuelPurchaseApproval.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/model/FuelPurchaseApproval.java
@@ -1,4 +1,34 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.model;
 
 public class FuelPurchaseApproval {
+    private String requests,approvalStatus;
+
+    public FuelPurchaseApproval(String requests, String approvalStatus) {
+        this.requests = requests;
+        this.approvalStatus = approvalStatus;
+    }
+
+    public String getRequests() {
+        return requests;
+    }
+
+    public void setRequests(String requests) {
+        this.requests = requests;
+    }
+
+    public String getApprovalStatus() {
+        return approvalStatus;
+    }
+
+    public void setApprovalStatus(String approvalStatus) {
+        this.approvalStatus = approvalStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "FuelPurchaseApproval{" +
+                "requests='" + requests + '\'' +
+                ", approvalStatus='" + approvalStatus + '\'' +
+                '}';
+    }
 }
