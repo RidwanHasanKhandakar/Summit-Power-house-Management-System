cde66157f17b9a7407e97bdf3171d92c5c120d0e
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-07-30 07:37:02 +0600

    Updated controller class for AddEquipmentViewController

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/controller/AddEquipmentViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/controller/AddEquipmentViewController.java
@@ -3,5 +3,27 @@ package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_s
 public class AddEquipmentViewController
 {
     @javafx.fxml.FXML
+    private TextField equipmentNameTextField;
+    @javafx.fxml.FXML
+    private ComboBox vendorComboBox;
+    @javafx.fxml.FXML
+    private TextField equipmentIdTextField;
+    @javafx.fxml.FXML
+    private ComboBox equipmentCategoryComboBox;
+    @javafx.fxml.FXML
+    private DatePicker purchaseDateDatePicker;
+    @javafx.fxml.FXML
+    private TextField quantityTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void addEquipmentButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void homeButtonOnAction(ActionEvent actionEvent) {
+    }
+}
