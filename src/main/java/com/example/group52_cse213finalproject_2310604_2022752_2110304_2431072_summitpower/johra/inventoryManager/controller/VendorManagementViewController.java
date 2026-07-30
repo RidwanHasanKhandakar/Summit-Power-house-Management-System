f6ceaec62bd7f17c5d375aabbc1a9c81bbee4b77
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-07-30 19:24:52 +0600

    Updated controller class for VendorManagement

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/controller/VendorManagementViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/controller/VendorManagementViewController.java
@@ -4,6 +4,7 @@ import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_su
 import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.inventoryManager.model.Vendor;
 import javafx.event.ActionEvent;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class VendorManagementViewController
 {
@@ -28,6 +29,7 @@ public class VendorManagementViewController
 
     @javafx.fxml.FXML
     public void initialize() {
+
     }
 
     @javafx.fxml.FXML
