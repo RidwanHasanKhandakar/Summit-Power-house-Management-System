1bba934591b2c9dc50b16aff6ade3e101451f856
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-08-01 04:05:46 +0600

    Wrote the Salary Payments File handler

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/fileHandler/accountant/SalaryPaymentsFileHandler.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/fileHandler/accountant/SalaryPaymentsFileHandler.java
@@ -1,4 +1,45 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.fileHandler.accountant;
 
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.model.FinancialStatement;
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.model.SalaryPayments;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.*;
+import java.util.ArrayList;
+
 public class SalaryPaymentsFileHandler {
+
+    private static final String FILE_PATH = "data/ridwan/accountant/financialStatement.bin";
+    public static void save(SalaryPayments salaryPayments) {
+        ObservableList<SalaryPayments> list = readAll();
+        list.add(salaryPayments);
+        try{
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH));
+            oos.writeObject(new ArrayList<>(list));
+            oos.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static ObservableList<SalaryPayments> readAll() {
+
+        File file = new File(FILE_PATH);
+        if (!file.exists()||file.length()==0) {
+            return FXCollections.observableArrayList();
+        }
+
+        try{
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH));
+            ArrayList<SalaryPayments> list = (ArrayList<SalaryPayments>) ois.readObject();
+            ois.close();
+            return FXCollections.observableArrayList(list);
+        }catch (Exception e){
+            e.printStackTrace();
+            return FXCollections.observableArrayList();
+        }
+
+    }
+
 }
