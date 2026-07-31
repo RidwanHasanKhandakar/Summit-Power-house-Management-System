59e11db863d635a4d7caa9fa2a4e3909e15c6e3d
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-07-31 22:38:50 +0600

    fixed the phone number field type

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/model/Accountant.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/ridwan/accountant/model/Accountant.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 public class Accountant extends User implements Serializable {
 
-    public Accountant(String firstName, String lastName, String address, int phoneNumber, String gender, LocalDate dateOfBirth, String userId, String email, String password, String role) {
+    public Accountant(String firstName, String lastName, String address, String phoneNumber, String gender, LocalDate dateOfBirth, String userId, String email, String password, String role) {
         super(firstName, lastName, address, phoneNumber, gender, dateOfBirth, userId, email, password, role);
     }
 
