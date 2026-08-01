5dc8c413fc946022cb8c659c8075acf005c865cc
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-08-01 13:05:58 +0600

    Added validation for create Parts issuance button for Parts Issuance

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/controller/PartsIssuanceViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/controller/PartsIssuanceViewController.java
@@ -1,9 +1,11 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.inventoryManager.controller;
 
 import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.PrimarySceneSwitcher;
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.inventoryManager.model.PartsIssuance;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class PartsIssuanceViewController
 {
@@ -14,7 +16,17 @@ public class PartsIssuanceViewController
     @javafx.fxml.FXML
     private TextField quantityTextField;
     @javafx.fxml.FXML
-    private TextField employeeIdTextField;
+    private TextField requesterEmployeeIdTextField;
+    @javafx.fxml.FXML
+    private TableColumn<PartsIssuance, String> requesterEmployeeIdCol;
+    @javafx.fxml.FXML
+    private TableColumn<PartsIssuance, String> equipmentIdCol;
+    @javafx.fxml.FXML
+    private TableView<PartsIssuance> showTableView;
+    @javafx.fxml.FXML
+    private TableColumn<PartsIssuance, String> quantityCol;
+    @javafx.fxml.FXML
+    private TableColumn<PartsIssuance, String> issueDateCol;
 
     @javafx.fxml.FXML
     public void initialize() {
@@ -26,6 +38,12 @@ public class PartsIssuanceViewController
                 "103",
                 "104"
         );
+
+        equipmentIdCol.setCellValueFactory(new PropertyValueFactory<>("equipmentId"));
+        quantityCol.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        requesterEmployeeIdCol.setCellValueFactory(new PropertyValueFactory<>("requesterEmployeeId"));
+        issueDateCol.setCellValueFactory(new PropertyValueFactory<>("issueDate"));
+
     }
 
     @javafx.fxml.FXML
@@ -35,5 +53,27 @@ public class PartsIssuanceViewController
 
     @javafx.fxml.FXML
     public void savePartsIssuanceButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void loadTableView(ActionEvent actionEvent) {
+    }
+
+    public void showError(String text){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(text);
+        alert.showAndWait();
+    }
+
+    @javafx.fxml.FXML
+    public void refreshButtonOnAction(ActionEvent actionEvent) {
+
+        equipmentIdComboBox.setValue(null);
+        requesterEmployeeIdTextField.clear();
+        quantityTextField.clear();
+        issueDateDatePicker.setValue(null);
+
     }
 }
