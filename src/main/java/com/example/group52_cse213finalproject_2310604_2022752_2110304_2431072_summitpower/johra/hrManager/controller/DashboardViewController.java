4a2a9734431407540dd6441ddeb445dcccf3aefe
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-08-01 11:21:06 +0600

    Updated dashboard controller

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/hrManager/controller/DashboardViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/hrManager/controller/DashboardViewController.java
@@ -2,17 +2,27 @@ package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_s
 
 import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.CurrentUser;
 import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.PrimarySceneSwitcher;
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.User;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 
 import java.util.Optional;
 
 public class DashboardViewController
 {
     @javafx.fxml.FXML
+    private Label officerNameLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
+        User user = CurrentUser.getUser();
+
+        if (user!=null){
+            officerNameLabel.setText(CurrentUser.getUser().getFirstName()+" "+CurrentUser.getUser().getLastName());
+        }
     }
 
     @javafx.fxml.FXML
@@ -27,11 +37,12 @@ public class DashboardViewController
 
     @javafx.fxml.FXML
     public void profileButtonOnAction(ActionEvent actionEvent) {
-        PrimarySceneSwitcher.primarySwitchScene((Node) actionEvent.getSource(), "johra", "hrManager", "employeePromotionManagement-view.fxml", "Manage Promotion");
+        PrimarySceneSwitcher.primarySwitchScene((Node) actionEvent.getSource(), "ridwan", "commonFiles", "profile-view.fxml", "Profile");
     }
 
     @javafx.fxml.FXML
     public void viewEmployeeDetailsButtonOnAction(ActionEvent actionEvent) {
+        PrimarySceneSwitcher.primarySwitchScene((Node) actionEvent.getSource(), "johra", "hrManager", "viewEmployeeDetails-view.fxml", "View Employee Details");
     }
 
     @javafx.fxml.FXML
