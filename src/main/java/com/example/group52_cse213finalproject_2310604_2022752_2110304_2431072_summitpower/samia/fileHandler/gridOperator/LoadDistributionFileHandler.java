f9dc8f3cd8c7e654058fed65c45308883e7aacc5
Samia Islam <2431072@iub.edu.bd>
2026-08-01 18:09:13 +0600

    made file handler for load distribution and monitor grid status class

---
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/samia/fileHandler/gridOperator/LoadDistributionFileHandler.java
@@ -0,0 +1,51 @@
+package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.fileHandler.gridOperator;
+
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.gridOperator.model.LoadDistribution;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class LoadDistributionFileHandler {
+    private static final String FILE_PATH =
+            "data/samia/gridOperator/LoadDistribution.bin";
+
+    public static void save(LoadDistribution loadDistribution) {
+
+        ObservableList<LoadDistribution> list = readAll();
+        list.add(loadDistribution);
+
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_PATH));
+            oos.writeObject(new ArrayList<>(list));
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static ObservableList<LoadDistribution> readAll() {
+
+        File file = new File(FILE_PATH);
+
+        if (!file.exists() || file.length() == 0) {
+            return FXCollections.observableArrayList();
+        }
+
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_PATH));
+            ArrayList<LoadDistribution> list =
+                    (ArrayList<LoadDistribution>) ois.readObject();
+            ois.close();
+
+            return FXCollections.observableArrayList(list);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return FXCollections.observableArrayList();
+        }
+    }
+
+}
