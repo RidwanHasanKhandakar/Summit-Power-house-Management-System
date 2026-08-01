05a1c8e8a52931f40e027a4e04bbb62aed91aaa6
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-08-01 13:50:18 +0600

    Added validatio for create notice button for CreateNotice controller

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/hrManager/controller/CreateNoticeViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/hrManager/controller/CreateNoticeViewController.java
@@ -14,9 +14,40 @@ public class CreateNoticeViewController
     private TextField subjectTextField;
     @javafx.fxml.FXML
     private TextArea contentTextArea;
+    @javafx.fxml.FXML
+    private TextField createdbyTextField;
+    @javafx.fxml.FXML
+    private TextField noticeIdTextField;
+    @javafx.fxml.FXML
+    private DatePicker expiryDateDatePicker;
 
     @javafx.fxml.FXML
     public void initialize() {
+
+        if(noticeIdTextField.getText().isEmpty()){
+            showError("Field cannot be empty");
+        }
+
+        if(subjectTextField.getText().isEmpty()){
+            showError("Field cannot be empty");
+        }
+
+        if(contentTextArea.getText().isEmpty()){
+            showError("Field cannot be empty");
+        }
+
+        if(createdbyTextField.getText().isEmpty()){
+            showError("Field cannot be empty");
+        }
+
+        if(publishDateDatePicker == null){
+            showError("Field cannot be empty");
+        }
+
+        if(expiryDateDatePicker == null){
+            showError("Field cannot be empty");
+        }
+
     }
 
     @javafx.fxml.FXML
@@ -26,5 +57,25 @@ public class CreateNoticeViewController
 
     @javafx.fxml.FXML
     public void createNoticeButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void showError(String text){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(text);
+        alert.showAndWait();
+    }
+
+    @javafx.fxml.FXML
+    public void refreshButtonOnAction(ActionEvent actionEvent) {
+
+        noticeIdTextField.clear();
+        subjectTextField.clear();
+        contentTextArea.clear();
+        createdbyTextField.clear();
+        publishDateDatePicker.setValue(null);
+        expiryDateDatePicker.setValue(null);
+
     }
 }
