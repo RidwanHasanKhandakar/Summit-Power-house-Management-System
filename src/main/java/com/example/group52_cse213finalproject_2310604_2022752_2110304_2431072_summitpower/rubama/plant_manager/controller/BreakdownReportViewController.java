a88fb43a3b36144dba54c11504a0b24964227521
rubamamehnaj <rubamamehnaj@gmail.com>
2026-08-02 12:24:04 +0600

    added a model class and updated the controller

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/plant_manager/controller/BreakdownReportViewController.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/plant_manager/controller/BreakdownReportViewController.java
@@ -1,32 +1,44 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.plant_manager.controller;
 
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.plant_manager.model.BreakdownReport;
 import javafx.event.ActionEvent;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.time.LocalDate;
 
 public class BreakdownReportViewController
 {
     @javafx.fxml.FXML
-    private TableView checkMaintenanceScheduleTableView;
+    private TableView <BreakdownReport> checkMaintenanceScheduleTableView;
     @javafx.fxml.FXML
     private DatePicker dateDatePicker;
     @javafx.fxml.FXML
-    private TableColumn dateCol;
+    private TableColumn <BreakdownReport, LocalDate> dateCol;
     @javafx.fxml.FXML
-    private TableColumn statusCol;
+    private TableColumn <BreakdownReport,String> statusCol;
     @javafx.fxml.FXML
-    private TableColumn unitNameCol;
+    private TableColumn <BreakdownReport,String> unitNameCol;
     @javafx.fxml.FXML
-    private ComboBox unitNameComboBox;
+    private ComboBox <String> unitNameComboBox;
     @javafx.fxml.FXML
-    private TableColumn unitNoCol;
+    private TableColumn <BreakdownReport,String> unitNoCol;
     @javafx.fxml.FXML
-    private ComboBox unitNoComboBox;
+    private ComboBox <String> unitNoComboBox;
 
     @javafx.fxml.FXML
     public void initialize() {
+        unitNameComboBox.getItems().addAll("Operations","Management","HR","Engineer","Accounts","Grid Operation");
+        unitNoComboBox.getItems().addAll("Op121","Mgt131","Hr141","Eng151","Acn161","Go171");
+
+        unitNameCol.setCellValueFactory(new PropertyValueFactory<>("unitName"));
+        unitNoCol.setCellValueFactory(new PropertyValueFactory<>("unitNo"));
+        statusCol.setCellValueFactory(new PropertyValueFactory<>("status"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<>("date"));
+
     }
 
     @javafx.fxml.FXML
--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/plant_manager/model/BreakdownReport.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/plant_manager/model/BreakdownReport.java
@@ -1,4 +1,58 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.plant_manager.model;
 
+import java.time.LocalDate;
+
 public class BreakdownReport {
+    private String unitName,unitNo,status;
+    private LocalDate date;
+
+    public BreakdownReport(String unitName, String unitNo, String status, LocalDate date) {
+        this.unitName = unitName;
+        this.unitNo = unitNo;
+        this.status = status;
+        this.date = date;
+    }
+
+    public String getUnitName() {
+        return unitName;
+    }
+
+    public void setUnitName(String unitName) {
+        this.unitName = unitName;
+    }
+
+    public String getUnitNo() {
+        return unitNo;
+    }
+
+    public void setUnitNo(String unitNo) {
+        this.unitNo = unitNo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "BreakdownReport{" +
+                "unitName='" + unitName + '\'' +
+                ", unitNo='" + unitNo + '\'' +
+                ", status='" + status + '\'' +
+                ", date=" + date +
+                '}';
+    }
 }
+
