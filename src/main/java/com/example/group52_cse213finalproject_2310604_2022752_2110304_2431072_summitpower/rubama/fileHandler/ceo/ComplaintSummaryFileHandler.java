cb32196213ef1c789fd6ab06e7dbaba6c263645c
rubamamehnaj <rubamamehnaj@gmail.com>
2026-08-04 21:19:05 +0600

    added filehandler for complaint summary

---
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/fileHandler/ceo/ComplaintSummaryFileHandler.java
@@ -0,0 +1,5 @@
+package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.fileHandler.ceo;
+
+public class ComplaintSummaryFileHandler {
+    private static final String FILE_PATH = "data/rubama/ceo/ComplaintSummary.bin";
+}
