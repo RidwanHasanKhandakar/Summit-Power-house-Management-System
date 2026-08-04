b60199866dade38ea743881466b324fc0a6e619f
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-08-04 20:56:26 +0600

    wrote all open lines for all the users in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,18 +6,32 @@ module com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_su
     exports com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower;
 
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower to javafx.fxml;
+
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.commonFiles to javafx.fxml;
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.controller to javafx.fxml;
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.bpdbOfficer.controller to javafx.fxml;
+
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.controller to javafx.fxml;
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.plant_manager.controller to javafx.fxml;
+
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.hrManager.controller to javafx.fxml;
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.inventoryManager.controller to javafx.fxml;
+
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.engineer.controller to javafx.fxml;
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.gridOperator.controller to javafx.fxml;
 
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.model to javafx.base;
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.bpdbOfficer.model to javafx.base;
 
+    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.gridOperator.model to javafx.base;
+    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.engineer.model to javafx.base;
+
+    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.hrManager.model to javafx.base;
+    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.inventoryManager.model to javafx.base;
+
+    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.model to javafx.base;
+    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.plant_manager.model to javafx.base;
+
+    //opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.
 
 }
