0efd423da852d07658693f752173e2fd966ef9ab
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-08-04 20:51:40 +0600

    fixed my user opens in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -16,4 +16,8 @@ module com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_su
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.engineer.controller to javafx.fxml;
     opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.samia.gridOperator.controller to javafx.fxml;
 
+    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.accountant.model to javafx.base;
+    opens com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.ridwan.bpdbOfficer.model to javafx.base;
+
+
 }
