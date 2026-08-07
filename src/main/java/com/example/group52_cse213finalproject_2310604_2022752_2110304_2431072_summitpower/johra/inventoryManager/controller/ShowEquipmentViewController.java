80a588b87219ea9a8ab821f2e200843daa3317a5
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-08-08 04:32:48 +0600

    Created JohraSceneSwitcher

---
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/controller/ShowEquipmentViewController.java
@@ -0,0 +1,27 @@
+package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.inventoryManager.controller;
+
+public class ShowEquipmentViewController
+{
+    @javafx.fxml.FXML
+    private TableColumn equipmentIdCol;
+    @javafx.fxml.FXML
+    private TableView showTableView;
+    @javafx.fxml.FXML
+    private TableColumn purchaseDateCol;
+    @javafx.fxml.FXML
+    private TableColumn quantityCol;
+    @javafx.fxml.FXML
+    private TableColumn equipmentNameCol;
+    @javafx.fxml.FXML
+    private TableColumn categoryCol;
+    @javafx.fxml.FXML
+    private TableColumn vendorCol;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void backButtonOnAction(ActionEvent actionEvent) {
+    }
+}
