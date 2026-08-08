7930ed9ba550d15e76356dd98d2844c7f19f3a09
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-08-09 03:18:41 +0600

    fixed the financial summary for ceo model

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/model/FinancialSummary.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/ceo/model/FinancialSummary.java
@@ -1,62 +1,87 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.ceo.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class FinancialSummary {
-    private String revenue;
-    private int fuelCost,operationalCost;
-    private LocalDate fromDate,toDate;
+public class FinancialSummary implements Serializable {
 
-    public FinancialSummary(String revenue, int fuelCost, int operationalCost, LocalDate fromDate, LocalDate toDate) {
+    private static final long serialVersionUID = 1L;
+
+    private String revenue;
+    private int fuelCost;
+    private int operationalCost;
+    private LocalDate fromDate;
+    private LocalDate toDate;
+
+
+    public FinancialSummary(
+            String revenue,
+            int fuelCost,
+            int operationalCost,
+            LocalDate fromDate,
+            LocalDate toDate) {
+
         this.revenue = revenue;
         this.fuelCost = fuelCost;
         this.operationalCost = operationalCost;
         this.fromDate = fromDate;
         this.toDate = toDate;
     }
+
 
     public String getRevenue() {
         return revenue;
     }
 
+
     public void setRevenue(String revenue) {
         this.revenue = revenue;
     }
+
 
     public int getFuelCost() {
         return fuelCost;
     }
 
+
     public void setFuelCost(int fuelCost) {
         this.fuelCost = fuelCost;
     }
+
 
     public int getOperationalCost() {
         return operationalCost;
     }
 
+
     public void setOperationalCost(int operationalCost) {
         this.operationalCost = operationalCost;
     }
+
 
     public LocalDate getFromDate() {
         return fromDate;
     }
 
+
     public void setFromDate(LocalDate fromDate) {
         this.fromDate = fromDate;
     }
+
 
     public LocalDate getToDate() {
         return toDate;
     }
 
+
     public void setToDate(LocalDate toDate) {
         this.toDate = toDate;
     }
 
+
     @Override
     public String toString() {
+
         return "FinancialSummary{" +
                 "revenue='" + revenue + '\'' +
                 ", fuelCost=" + fuelCost +
