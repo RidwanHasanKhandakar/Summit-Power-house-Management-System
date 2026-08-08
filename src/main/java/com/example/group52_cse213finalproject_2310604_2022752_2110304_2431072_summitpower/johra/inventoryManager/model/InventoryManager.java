48fecb905db716d114482a3433a048d839d707f1
Kazi Fatema Tuj Johra <kazifatematujohra@gmail.com>
2026-08-09 03:07:02 +0600

    Done and dusted!

---
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/johra/inventoryManager/model/InventoryManager.java
@@ -0,0 +1,18 @@
+package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.johra.inventoryManager.model;
+
+import com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.User;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+public class InventoryManager extends User implements Serializable {
+
+    public InventoryManager(String firstName, String lastName, String address, String phoneNumber, String gender, LocalDate dateOfBirth, String userId, String email, String password, String role) {
+        super(firstName, lastName, address, phoneNumber, gender, dateOfBirth, userId, email, password, role);
+    }
+
+    @Override
+    public String toString() {
+        return "InventoryManager{}";
+    }
+}
