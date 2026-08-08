bc133415b7090bdbe0290bcf6c9b08bef9df1ccc
Ridwan Hasan Khandakar <2310604@iub.edu.bd>
2026-08-09 03:58:28 +0600

    fixed the code for breakdownReport Controller &breakdownReprt model class

--- src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/plant_manager/model/BreakdownReport.java
+++ src/main/java/com/example/group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower/rubama/plant_manager/model/BreakdownReport.java
@@ -1,9 +1,15 @@
 package com.example.group52_cse213finalproject_2310604_2022752_2110304_2431072_summitpower.rubama.plant_manager.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class BreakdownReport {
-    private String unitName,unitNo,status;
+public class BreakdownReport implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String unitName;
+    private String unitNo;
+    private String status;
     private LocalDate date;
 
     public BreakdownReport(String unitName, String unitNo, String status, LocalDate date) {
@@ -54,5 +60,6 @@ public class BreakdownReport {
                 ", date=" + date +
                 '}';
     }
-}
 
+
+}
